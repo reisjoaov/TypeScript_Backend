cdce376263e372152f4d9b12cbb81b08c61cc222
reisjoaov <joaoreisvictor@hotmail.com>
2025-08-08 13:56:35 -0300

    CRUD completo, sem DB

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
-//CRUD     - READ
-import {usuarios, UsuarioType}  from './usuarios';
-
-const usuariosBD = retornaUsuarios();
-console.log('usuariosBD',usuariosBD);
-
-const usuarios1 = retornaUsuarioPorId(1232345);
-mostraEmTela()
-console.log('usuarios1',usuarios1);
+//CRUD - READ
+import {Usuario, usuarios, UsuarioSchema}  from './usuarios';
 
 function mostraEmTela(): void {
     console.log("exibindo em tela");
@@ -16,9 +9,47 @@ function mostraEmTela(): void {
 function retornaUsuarios () {
     return usuarios;
 }
-function retornaUsuarioPorId (id: number): UsuarioType | undefined {
+function retornaUsuarioPorId (id: number): UsuarioSchema | undefined {
     console.log('tipo do id =', typeof id);
     return usuarios.find(user => user.id === id);
 }
 
-// criar atualiza e delete
+//CRUD - CREATE
+function criarUsuario(usuario: Usuario): UsuarioSchema[]{
+    usuarios.push({...usuario});
+    return usuarios;
+}
+const usuario = new Usuario('Paulo', true, 1230n);
+console.log(criarUsuario(usuario));
+
+
+//CRUD - UPDATE
+function atualizarUsuario (id: number, dadosAtualizacao: UsuarioSchema): UsuarioSchema | undefined {
+    const indiceUsuario = usuarios.findIndex(user => user.id ===id);
+
+    if (indiceUsuario === -1){
+        console.log(`Usuário com ID ${id} não encontrado`);
+        return;
+    }
+
+    //Atualiza apenas os campos fornecidos
+    usuarios[indiceUsuario] = {
+        ...usuarios[indiceUsuario],
+        ...dadosAtualizacao
+    };
+
+    return usuarios[indiceUsuario];
+}
+
+//CRUD - DELETE
+function deletaUsuario(id: number): UsuarioSchema | undefined{
+    const indiceUsuario = usuarios.findIndex(user => user.id === id);
+
+    if (indiceUsuario === -1){
+        console.log (`Usuário com ID ${id} não encontrado`);
+    }
+
+    //Remove o usuário do Array e retorno o usuário removido
+    const usuarioRemovido = usuarios.splice(indiceUsuario, 1)[0];
+    return usuarioRemovido;
+}
--- src/usuarios.ts
+++ src/usuarios.ts
@@ -1,9 +1,9 @@
-export const usuarios: UsuarioType[] = [
+export const usuarios: UsuarioSchema[] = [
     { 
        id: 123,
        nome: 'Renan',
        ativo: false,
-       saldo: 124398498578n,
+       saldo: 124398498578n
     },
     { 
        id: 124,
@@ -12,9 +12,27 @@ export const usuarios: UsuarioType[] = [
     },
 ]
 
-export type UsuarioType = {
+export type UsuarioSchema = {
     id: number,
     nome: string,
     ativo: boolean,
     saldo?: bigint
+}
+
+export class Usuario {
+    id: number;
+    nome: string;
+    ativo: boolean = true;
+    saldo?: bigint = 12n;
+
+    constructor(nome: string, ativo: boolean, saldo?: bigint) {
+        this.id = Math.round (Math.random()*100);
+        this.nome = nome;
+        this.ativo = ativo;
+        this.saldo = saldo;
+    }
+
+    getNome() {
+        return this.nome;
+    }
 }
